af27c5f3ad2dead804aeb1719afab234c1cc9a25
Eamonn-wooster <167955744+Eamonn-wooster@users.noreply.github.com>
2026-05-12 11:50:12 +1000

    name update

--- Tree/Phylo_Tree_.R
+++ Tree/Phylo_Tree_.R
@@ -44,15 +44,23 @@ setDT(Species)
 
 str(Species)
 
+#Few names changes 
+
+Species$Species <- gsub("Equus_quagga","Equus_burchellii_quagga", Species$Species)
+Species$Species <- gsub("Macropus_eugenii","Notamacropus_eugenii",  Species$Species)
+Species$Species <- gsub("Hyla_versicolor", "Dryophytes_versicolor",  Species$Species)
+Species$Species <- gsub("Bufo_americanus", "Anaxyrus_americanus",  Species$Species)
+Species$Species <- gsub( "Gambusiaa_hubbsi","Gambusia_hubbsi",  Species$Species)
+
 
 # Now pass the character vector
-matches <- tnrs_match_names(species_names)
+resolved_spp <- tnrs_match_names(Species$Species, context_name = "Animals")
 
 # Look at the matches
-print(matches)
+resolved_spp
 
 # Extract ott_ids
-ott_ids <- matches$ott_id
+ott_ids <- resolved_spp$ott_id
 
 # Remove NA ott_ids
 clean_ott_ids <- ott_ids[!is.na(ott_ids)]
@@ -60,68 +68,12 @@ clean_ott_ids <- ott_ids[!is.na(ott_ids)]
 # Now check which are in the tree
 is_in_tree(clean_ott_ids)
 
-#Checking for failed matches
-failed_matches <- matches[is.na(matches$ott_id), ]
+#Checking for failed matches - NONE
+failed_matches <- resolved_spp[is.na(resolved_spp$ott_id), ]
 print(failed_matches$search_string)
 
-##Changing the names of the species that failed to match ##
 
-Species <- Species[Study_Species == "Sylvilagus bachmani riparius", Study_Species := "Sylvilagus bachmani"]
-
-Species <- Species[Study_Species == "Sciurus nayaritensis chiricahuae", Study_Species := "Sciurus nayaritensis"]
-
-dat <- dat %>%
-  mutate(Study_Species = if_else(
-    Study_Species == "Sylvilagus bachmani riparius",
-    "Sylvilagus bachmani",
-    Study_Species
-  ))
-
-dat <- dat %>%
-  mutate(Study_Species = if_else(
-    Study_Species == "Sciurus nayaritensis chiricahuae",
-    "Sciurus nayaritensis",
-    Study_Species
-  ))
-
-## Re-running tnrs_match_names after changing the above names ##
-
-species_names <- Species$Study_Species  # pulls out the character vector
-
-str(species_names)
-
-# Now pass the character vector
-matches <- tnrs_match_names(species_names)
-
-# Look at the matches
-print(matches)
-
-# Extract ott_ids
-ott_ids <- matches$ott_id
-
-# Remove NA ott_ids
-clean_ott_ids <- ott_ids[!is.na(ott_ids)]
-
-# Now check which are in the tree
-is_in_tree(clean_ott_ids)
-
-#Checking for failed matches
-failed_matches <- matches[is.na(matches$ott_id), ]
-print(failed_matches$search_string)
-
-## Name changes worked ##
-
-Species <- unique(Species)
-
-#Reaching out to tree to see matches - make sure all your species have a correct match
-
-resolved_Species <- tnrs_match_names(Species$Study_Species, context_name = "Animals")
-
-resolved_Species
-
-length(resolved_Species$ott_id)
-
-Species_tree <- tol_induced_subtree(ott_ids = resolved_Species$ott_id)
+Species_tree <- tol_induced_subtree(ott_ids = resolved_spp$ott_id)
 
 length(Species_tree$tip.label)
 
